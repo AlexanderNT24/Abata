b89567a4b4aa6a925a3424dbe93d8495bf7b26c6
Alexander <89727568+AlexanderNT24@users.noreply.github.com>
2022-01-19 01:01:56 -0500

    agregue analisis facial y emociones

--- public/src/facemesh.js
+++ public/src/facemesh.js
@@ -1,6 +1,9 @@
 const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
+canvasCtx.translate(1280, 0);
+canvasCtx.scale(-1, 1);
+
 document.querySelector('#inputVid').style.display = 'none';
 function onResults(results) {
     canvasCtx.save();
@@ -46,40 +49,98 @@ document.getElementById("botonStreamingStop").onclick = () => {
     document.getElementById("video").style.display = "none";
 };
 function analysisFacial(landmarks) {
-    const distanceEyeRight = distancePoints(landmarks[159]["x"], landmarks[159]["y"], landmarks[145]["x"], landmarks[145]["y"]);
-    const distanceEyeLeft = distancePoints(landmarks[386]["x"], landmarks[386]["y"], landmarks[374]["x"], landmarks[374]["y"]);
-    var eyeRight = "", eyeLeft = "";
-    var colorLeft = "", colorRight = "";
-    if (distanceEyeLeft < 10) {
-        eyeLeft = "cerrado";
-        colorLeft = '#FF3030';
+    var percentage = (Math.hypot(landmarks[323]["x"] - landmarks[93]["x"], landmarks[323]["y"] - landmarks[93]["y"]) * 1000) / 240;
+    const distanceEyeRight = distancePoints(landmarks, percentage, 159, 145);
+    const distanceEyeLeft = distancePoints(landmarks, percentage, 386, 374);
+    const distanceMouth = distancePoints(landmarks, percentage, 13, 14);
+    const distanceEyeBrowLeft = distancePoints(landmarks, percentage, 386, 282);
+    const distanceEyeBrowRight = distancePoints(landmarks, percentage, 159, 52);
+    const distanceMouthHorizontal=distancePoints(landmarks, percentage, 61, 291);
+    var rotation = "null";
+    if (landmarks[9]["x"] - landmarks[152]["x"]) {
+        const earring = (landmarks[9]["y"] - landmarks[152]["y"]) / (landmarks[9]["x"] - landmarks[152]["x"]);
+        if (earring < -0.1 && earring > -4) {
+            rotation = "right";
+        }
+        else if (earring > 0.1 && earring < 4) {
+            rotation = "left";
+        }
+    }
+    var eyeRight = "", eyeLeft = "", mouth = "", eyeBrowLeft = "", eyeBrowRight = "";
+    var color = "";
+    if (distanceEyeLeft < 12) {
+        eyeLeft = "close";
+        color = '#FF3030';
     }
     else {
-        eyeLeft = "abierto";
-        colorLeft = '#30FF30';
+        eyeLeft = "open";
+        color = '#30FF30';
     }
-    drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYE, { color: colorLeft });
+    drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYE, { color: color });
 
-    if (distanceEyeRight < 10) {
-        eyeRight = "cerrado";
-        colorRight = '#FF3030';
+    if (distanceEyeRight < 12) {
+        eyeRight = "close";
+        color = '#FF3030';
     }
     else {
-        eyeRight = "abierto";
-        colorRight = '#30FF30';
+        eyeRight = "open";
+        color = '#30FF30';
     }
-    drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, { color: colorRight });
+    drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, { color: color });
 
-    // drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYEBROW, { color: '#FF3030' });
+    if (distanceMouth < 5) {
+        mouth = "close";
+        color = '#FF3030';
+    }
+    else {
+        mouth = "open";
+        color = '#30FF30';
+    }
+    drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, { color: color });
+
+    if (distanceEyeBrowLeft < 65) {
+        eyeBrowLeft = "down";
+        color = '#FF3030';
+    } else {
+        eyeBrowLeft = "up";
+        color = '#30FF30';
+    }
+    drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYEBROW, { color: color });
+
+    if (distanceEyeBrowRight < 65) {
+        eyeBrowRight = "down";
+        color = '#FF3030';
+    } else {
+        eyeBrowRight = "up";
+        color = '#30FF30';
+    }
+    drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYEBROW, { color: color });
+
+
     // drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_IRIS, { color: '#FF3030' });
 
-    // drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYEBROW, { color: '#30FF30' });
     // drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_IRIS, { color: '#30FF30' });
     // drawConnectors(canvasCtx, landmarks, FACEMESH_FACE_OVAL, { color: '#E0E0E0' });
-    // drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, { color: '#E0E0E0' });
-    document.querySelector('#data').textContent = "Ojos: derecho {" + eyeRight + "} izquierdo {" + eyeLeft + "}     ";
-    console.log("Distance: " + distanceEyeLeft);
+    const facialData="Eyes: right {" + eyeRight + "} left {" + eyeLeft + "} EyeBrow: right {" + eyeBrowRight + "} left {" + eyeBrowLeft + "} Mouth {" + mouth + "} Rotation {"+rotation+"}";
+    const emotion="Emotion: "+ getEmotion(distanceEyeBrowLeft,distanceEyeBrowRight,distanceMouthHorizontal,distanceMouth);
+    document.querySelector('#data').textContent = facialData;
+    document.querySelector('#dataEmotion').textContent = emotion;
 }
-function distancePoints(x1, y1, x2, y2) {
-    return Math.hypot(x2 - x1, y2 - y1) * 1000;
+function getEmotion(distanceEyeBrowLeft,distanceEyeBrowRight,distanceMouthHorizontal,distanceMouth){
+    var emotion="null";
+    console.log(distanceMouthHorizontal);
+    if((distanceEyeBrowRight>65 || distanceEyeBrowLeft<65) && (distanceMouthHorizontal>120)){
+        emotion="happy";
+    } else
+    if((distanceEyeBrowLeft<=37 || distanceEyeBrowRight<=37)){
+        emotion="angry";
+    } else 
+    if((distanceEyeBrowLeft>65 || distanceEyeBrowRight>65) && (distanceMouth>10)){
+        emotion="amazed";
+    }
+    return emotion;
+    
+}
+function distancePoints(landmarks, percentage, point1, point2) {
+    return Math.abs(parseInt((Math.hypot(landmarks[point2]["x"] - landmarks[point1]["x"], landmarks[point2]["y"] - landmarks[point1]["y"]) * 1000) / percentage));
 }
